aeaa7069498e4b9c2946792b50fe8c198a3faf6b
Steven Xu <xxu@student.unimelb.edu.au>
2015-09-30 17:01:56 +1000

    moved data to /data/ so we have the same paths, also don't think we really need a class for this

---
+++ project2/data/conv_csv.py
@@ -0,0 +1,89 @@
+
+import csv
+
+from sklearn.feature_extraction import DictVectorizer
+
+
+def conv(in_sr, out_sr):
+    '''
+    This method converts a csv file containing all the features of every suburb
+    into a scikit-learn readable data structures. First, we turn the data into
+    dictionaries (e.g. {'Community Name': 'Ascot Vale (Suburb)', ... ,'Pharmacies': 4, ...}).
+    Then we convert it using scikit-learn's DictVectorizer.
+
+    Note that:
+        - Feature 'Location' is split into two features:
+            1. Location (distance km) - records the distance in km
+            2. Location (direction) - records the direction (e.g. NW)
+        - Some continuous values have the value '<5', this was turned into 1
+        - 'n/a' values were turned into None
+    '''
+    categorical_feats = [
+        'Region',
+        'Map reference',
+        'Grid reference',
+        'Location (Direction)',
+        'LGA',
+        'Primary Care Partnership',
+        'Medicare Local',
+        'DHS Area',
+        'Top industry',
+        '2nd top industry - persons',
+        '3rd top industry - persons',
+        'Top occupation',
+        '2nd top occupation - persons',
+        '3rd top occupation - persons',
+        'Top country of birth',
+        '2nd top country of birth',
+        '3rd top country of birth',
+        '4th top country of birth',
+        '5th top country of birth',
+        'Top language spoken',
+        '2nd top language spoken',
+        '3rd top language spoken',
+        '4th top language spoken',
+        '5th top language spoken',
+        'Nearest Public Hospital',
+        'Nearest public hospital with maternity services',
+        'Nearest public hospital with emergency department',
+    ]
+    reader = csv.reader(in_sr)
+    feature_names = next(reader)
+    matrix = []
+    for line in reader:
+        instance = {}
+        for i, feat_value in enumerate(line):
+            if feature_names[i] == 'Location':
+                feat_values = feat_value.split()
+                distance = feat_values[0]
+                direction = feat_values[1]
+                instance['Location (distance km)'] = float(distance.replace('km', ''))
+                instance['Location (direction)'] = direction
+                continue
+            if feature_names[i] in categorical_feats or i == 0:
+                if feat_value == 'n/a':
+                    instance[feature_names[i]] = None
+                else:
+                    instance[feature_names[i]] = feat_value
+            else:
+                if '<' in feat_value:
+                    feat_value = '1'
+                feat_value = feat_value.replace(',', '')
+                if feat_value == 'n/a':
+                    instance[feature_names[i]] = None
+                else:
+                    instance[feature_names[i]] = float(feat_value)
+        matrix.append(instance)
+    vec = DictVectorizer()
+    vec.fit_transform(matrix).toarray()
+    writer = csv.writer(out_sr)
+    writer.writerow(vec.get_feature_names())
+    writer.writerows(vec.fit_transform(matrix).toarray())
+
+
+def main():
+    with open('data/matrix_mod.csv') as in_sr, open('data/output.csv', 'w') as out_sr:
+        conv(in_sr, out_sr)
+
+if __name__ == '__main__':
+    main()
