75e584489b573c11673dc2c0ed37ceb2e6238066
Steven Xu <xxu@student.unimelb.edu.au>
2015-10-13 00:57:22 +1100

    filter correlated features

--- project2/analysis/mds.py
+++ project2/analysis/mds.py
@@ -303,12 +303,18 @@ def main():
         'Category 4 & 5 emergency department presentations, %',
     ]
     # endregion
-
     with open('data/binarized.pickle', 'rb') as pickle_sr:
         labels, names, categorical_names, numeric_names, X = pickle.load(pickle_sr)
+        with open('data/correlations_filtered.csv') as sr:
+            keep_names = csv.DictReader(sr).fieldnames
+            keep_rows = set(
+                i for i, name in enumerate(names) if any(name.startswith(keep_name) for keep_name in keep_names)
+            )
+            # print removed names
+            # print('\n'.join(names[i] for i in sorted(set(range(len(names))) - keep_rows)))
         group = group_1
-        rows = list(i for i, name in enumerate(names) if any(name.startswith(group_name) for group_name in group))
-        X = normalize_instances(X, names, numeric_names)[:, rows]
+        rows = set(i for i, name in enumerate(names) if any(name.startswith(group_name) for group_name in group))
+        X = normalize_instances(X, names, numeric_names)[:, sorted(rows & keep_rows)]
         mult_scl(X, labels)
 
 if __name__ == '__main__':
